67dcdeabb2952f6a4d601b9e28abbbc5140ba266
Shashank Prakash <98454860+anonymous0905@users.noreply.github.com>
2024-10-15 12:25:00 +0530

    statute-rabbitmq-working

--- Statute_verification/statute_verification.py
+++ Statute_verification/statute_verification.py
@@ -7,7 +7,8 @@ channel = connection.channel()
 
 channel.queue_declare(queue='statute_verification_health')
 channel.queue_declare(queue='statute_verification_health_return')
-
+channel.queue_declare(queue='statute')
+channel.queue_declare(queue='statute_return')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
@@ -17,9 +18,23 @@ def callback(ch, method, properties, body):
     resp = json.dumps(message,default='str')
     channel.basic_publish(exchange='', routing_key='statute_verification_health_return', body=resp)
 
+def callback_statute(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    print(" [x] Done")
+    # ch.basic_ack(delivery_tag = method.delivery_tag)
+    #extract the message from the body
+    message = json.loads(body)
+    print(message)
+    text = message['inputData']
+    print(text)
+    #TODO: Implement statute verification logic here
+    message = '200 OK - Statute verified successfully'
+    resp = json.dumps(message,default='str')
+    channel.basic_publish(exchange='', routing_key='statute_return', body=resp)
 
 def main():
     channel.basic_consume(queue='statute_verification_health', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='statute', on_message_callback=callback_statute, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
